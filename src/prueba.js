3c432358388550521a6e6b8c84aa26c3fd9e14f1
jaaferna <jaaferna@bancolombia.com.co>
2024-11-26 20:29:35 -0500

    Modificación url consumo Render

--- src/prueba.js
+++ src/prueba.js
@@ -1,15 +1,30 @@
 class myElement extends HTMLElement {
     constructor() {
         super();
-        this.attachShadow({mode: "open"});
+        this.attachShadow({ mode: "open" });
+
+        this.img = this.getAttribute('img');
+
+    }
+    static get observedAttributes() {
+        return ["title", "parrafo"];
+    }
+    attributeChangedCallback(attr, oldVal, newVal) {
+        if (attr === "title" && oldVal !== newVal) {
+            this.title = newVal;
+        }
+
+        // oldVal !== newVal ? this[attr] = newVal : null;
     }
     getTemplate() {
         const template = document.createElement('template');
         template.innerHTML = `
         <div>
+            <h1>${this.title}</h1>
             <h2>
                 <slot name="prueba"></slot>
             </h2>
+            <img src=${this.img} />
         </div>
         ${this.getStyles()}
         `;
